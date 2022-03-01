da69c106949ec1a595ae0b01526ca332da73ea10
zerodayrat <bolt7w7@gmail.com>
2022-03-01 20:45:18 +0530

    🚀 RELEASE: Release 1.0.0

---
+++ src/script.js
@@ -0,0 +1,252 @@
+function _0x5c0d() {
+  var _0xea70ac = [
+    "https://www.google.com/search?q=+inurl:",
+    "https://www.reddit.com/search/?q=",
+    "https://censys.io/ipv4?q=",
+    "https://www.google.com/search?q=site:pastebin.com+",
+    "https://www.shodan.io/search?query=",
+    "+inurl:\x22/phpinfo.php\x22+|+inurl:\x22.htaccess\x22+|+inurl:\x22/.git\x22+",
+    "https://web.archive.org/web/*/",
+    "http://threatcrowd.org/domain.php?domain=",
+    "value",
+    "287120LtftJP",
+    "+intext:\x22sql+syntax+near\x22+|+intext:\x22syntax+error+has+occurred\x22+|+intext:\x22incorrect+syntax+near\x22+|+intext:\x22unexpected+end+of+SQL+command\x22+|+intext:\x22Warning:+mysql_connect()\x22+|+intext:\x22Warning:+mysql_query()\x22+|+intext:\x22Warning:+pg_connect()\x22",
+    "\x22&type=host",
+    "&source=recent",
+    "%20mime:swf",
+    "220xAOYkf",
+    "407646RjuzGL",
+    "target",
+    "\x20-github",
+    "+ext:xml+|+ext:conf+|+ext:cnf+|+ext:reg+|+ext:inf+|+ext:rdp+|+ext:cfg+|+ext:txt+|+ext:ora+|+ext:ini",
+    "+ext:doc+|+ext:docx+|+ext:odt+|+ext:pdf+|+ext:rtf+|+ext:sxw+|+ext:psw+|+ext:ppt+|+ext:pptx+|+ext:pps+|+ext:csv",
+    "8KNnTeo",
+    "+inurl:shell+|+inurl:backdoor+|+inurl:wso+|+inurl:cmd+|+shadow+|+passwd+|+boot.ini+|+inurl:backdoor",
+    "https://censys.io/domain?q=",
+    "https://yandex.com/search/?text=site:",
+    "2209144febvTr",
+    "+ext:action+|+ext:struts+|+ext:do",
+    "/&matchType=domain&collapse=urlkey&output=text&fl=original&filter=urlkey:.*swf&limit=100000&_=1507209148310",
+    "https://www.google.com/search?q=",
+    "://",
+    "https://www.google.com/search?q=site:linkedin.com+employees+",
+    "https://web.archive.org/cdx/search?url=",
+    "45604280ReAPqo",
+    "https://crt.sh/?q=%25.",
+    "2477469CImnsX",
+    "\x22intitle:index\x20of\x22\x20\x22/etc/mysql/\x22",
+    "http://",
+    "site:",
+    "http://wwwb-dedup.us.archive.org:8083/cdx/search?url=",
+    "/&matchType=domain&collapse=digest&output=text&fl=original,timestamp&filter=urlkey:.*wp[-].*&limit=1000000&xx=",
+    "open",
+    "getElementById",
+    "4185504WJwSiJ",
+    "79195eprseW",
+    "+ext:bkf+|+ext:bkp+|+ext:bak+|+ext:old+|+ext:backup",
+    "119cDJnpH",
+    "+inurl:redir+|+inurl:url+|+inurl:redirect+|+inurl:return+|+inurl:src=http+|+inurl:r=http",
+    "+inurl:readme+|+inurl:license+|+inurl:install+|+inurl:setup+|+inurl:config",
+    "+ext:sql+|+ext:dbf+|+ext:mdb",
+    "/&matchType=domain&collapse=urlkey&output=text&fl=original&filter=mimetype:application/x-shockwave-flash&limit=100000&_=1507209148310",
+    "inurl:admin\x20inurl:userlist",
+    "\x22inurl:/wp-content/uploads/\x20ext:txt\x20\x22username\x22\x20|\x20\x22user\x20name”\x20|\x20\x22uname\x22\x20|\x20\x22user\x22\x20|\x20\x22userid\x22\x20|\x20\x22user\x20id\x22\x20AND\x20\x22password\x22\x20|\x20\x22pass\x20word\x22\x20|\x20\x22pwd\x22\x20|\x20\x22pw\x22",
+    "+inurl:wp-content+|+inurl:wp-includes",
+    "inurl:/wwwboard/passwd.txt",
+    "&type=host",
+    "https://www.google.com/search?q=site:*.*.",
+    "+ext:log",
+  ];
+  _0x5c0d = function () {
+    return _0xea70ac;
+  };
+  return _0x5c0d();
+}
+function _0x1c47(_0x5bc4f2, _0x22781e) {
+  var _0x5c0d68 = _0x5c0d();
+  return (
+    (_0x1c47 = function (_0x1c47d2, _0xfc257c) {
+      _0x1c47d2 = _0x1c47d2 - 0x15c;
+      var _0x2c3764 = _0x5c0d68[_0x1c47d2];
+      return _0x2c3764;
+    }),
+    _0x1c47(_0x5bc4f2, _0x22781e)
+  );
+}
+(function (_0x53adaa, _0x52c2b5) {
+  var _0x2e7d85 = _0x1c47,
+    _0x1cddf8 = _0x53adaa();
+  while (!![]) {
+    try {
+      var _0x4f5896 =
+        (-parseInt(_0x2e7d85(0x17f)) / 0x1) *
+          (parseInt(_0x2e7d85(0x18a)) / 0x2) +
+        -parseInt(_0x2e7d85(0x15f)) / 0x3 +
+        (-parseInt(_0x2e7d85(0x184)) / 0x4) *
+          (parseInt(_0x2e7d85(0x168)) / 0x5) +
+        (-parseInt(_0x2e7d85(0x185)) / 0x6) *
+          (parseInt(_0x2e7d85(0x16a)) / 0x7) +
+        -parseInt(_0x2e7d85(0x18e)) / 0x8 +
+        parseInt(_0x2e7d85(0x167)) / 0x9 +
+        parseInt(_0x2e7d85(0x15d)) / 0xa;
+      if (_0x4f5896 === _0x52c2b5) break;
+      else _0x1cddf8["push"](_0x1cddf8["shift"]());
+    } catch (_0x3ad92c) {
+      _0x1cddf8["push"](_0x1cddf8["shift"]());
+    }
+  }
+})(_0x5c0d, 0xb6d60),
+  (dork = (_0x5f5d1b) => {
+    var _0x2ead18 = _0x1c47,
+      _0x2ba42e = document[_0x2ead18(0x166)](_0x2ead18(0x186))[
+        _0x2ead18(0x17e)
+      ],
+      _0x3a8b99 = _0x2ead18(0x191);
+    if (_0x2ba42e["includes"](_0x2ead18(0x192))) {
+      let _0x48cb3e = _0x2ba42e["replace"](/[a-zA-Z0-9]+:\/\//g, "");
+      url = _0x3a8b99 + _0x2ead18(0x162) + _0x48cb3e + "\x20";
+    } else url = _0x3a8b99 + "site:" + _0x2ba42e + "\x20";
+    switch (_0x5f5d1b) {
+      case 0x1:
+        url += "inurl:api_key";
+        break;
+      case 0x2:
+        url += _0x2ead18(0x188);
+        break;
+      case 0x3:
+        url += _0x2ead18(0x16d);
+        break;
+      case 0x4:
+        url += _0x2ead18(0x175);
+        break;
+      case 0x5:
+        url += _0x2ead18(0x169);
+        break;
+      case 0x6:
+        url += "+inurl:login";
+        break;
+      case 0x7:
+        url += _0x2ead18(0x180);
+        break;
+      case 0x8:
+        url += _0x2ead18(0x189);
+        break;
+      case 0x9:
+        url += "+ext:php+intitle:phpinfo+\x22published+by+the+PHP+Group\x22";
+        break;
+      case 0xa:
+        url +=
+          "+inurl:wp-+|+inurl:wp-content+|+inurl:plugins+|+inurl:uploads+|+inurl:themes+|+inurl:download";
+        break;
+      case 0xb:
+        url += _0x2ead18(0x18b);
+        break;
+      case 0xc:
+        url += _0x2ead18(0x16c);
+        break;
+      case 0xd:
+        url += _0x2ead18(0x16b);
+        break;
+      case 0xe:
+        url += _0x2ead18(0x18f);
+        break;
+      case 0xf:
+        url = _0x2ead18(0x179) + _0x2ba42e;
+        break;
+      case 0x10:
+        url = _0x2ead18(0x193) + _0x2ba42e;
+        break;
+      case 0x11:
+        url += _0x2ead18(0x17b) + _0x2ba42e + _0x2ead18(0x187);
+        break;
+      case 0x12:
+        url = "https://www.google.com/search?q=site:*." + _0x2ba42e;
+        break;
+      case 0x13:
+        url = _0x2ead18(0x174) + _0x2ba42e;
+        break;
+      case 0x14:
+        url += _0x2ead18(0x171);
+        break;
+      case 0x15:
+        url =
+          "https://github.com/search?q=\x22*." + _0x2ba42e + _0x2ead18(0x181);
+        break;
+      case 0x16:
+        url = _0x2ead18(0x161) + _0x2ba42e + "/crossdomain.xml";
+        break;
+      case 0x17:
+        url = _0x2ead18(0x17d) + _0x2ba42e;
+        break;
+      case 0x18:
+        url = _0x2ead18(0x176) + _0x2ba42e + "+ext:swf";
+        break;
+      case 0x19:
+        url = _0x2ead18(0x18d) + _0x2ba42e + _0x2ead18(0x183);
+        break;
+      case 0x1a:
+        url = _0x2ead18(0x15c) + _0x2ba42e + _0x2ead18(0x190);
+        break;
+      case 0x1b:
+        url = _0x2ead18(0x15c) + _0x2ba42e + _0x2ead18(0x16e);
+        break;
+      case 0x1c:
+        url = "https://web.archive.org/web/*/(." + _0x2ba42e + ")";
+      case 0x1d:
+        url = _0x2ead18(0x17c) + _0x2ba42e + "/*";
+        break;
+      case 0x1e:
+        url = _0x2ead18(0x15e) + _0x2ba42e;
+        break;
+      case 0x1f:
+        url =
+          "https://www.openbugbounty.org/search/?search=" +
+          _0x2ba42e +
+          _0x2ead18(0x173);
+        break;
+      case 0x20:
+        url = _0x2ead18(0x177) + _0x2ba42e + _0x2ead18(0x182);
+        break;
+      case 0x21:
+        url = _0x2ead18(0x163) + _0x2ba42e + _0x2ead18(0x164);
+        break;
+      case 0x22:
+        url = _0x2ead18(0x178) + _0x2ba42e;
+        break;
+      case 0x23:
+        url = _0x2ead18(0x18c) + _0x2ba42e;
+        break;
+      case 0x24:
+        url = "https://censys.io/certificates?q=" + _0x2ba42e;
+        break;
+      case 0x25:
+        url = _0x2ead18(0x17a) + _0x2ba42e;
+        break;
+      case 0x26:
+        url += "+intitle:index.of";
+        break;
+      case 0x27:
+        url += "inurl:email";
+        break;
+      case 0x28:
+        url += _0x2ead18(0x172);
+        break;
+      case 0x29:
+        url += _0x2ead18(0x160);
+        break;
+      case 0x2a:
+        url += "database.yml";
+        break;
+      case 0x2b:
+        url += _0x2ead18(0x16f);
+        break;
+      case 0x2c:
+        url +=
+          "inurl:admin\x20filetype:asp\x20inurl:userlist\x20-\x20lists\x20username";
+        break;
+      case 0x2d:
+        url += _0x2ead18(0x170);
+        break;
+    }
+    window[_0x2ead18(0x165)](url);
+  });
